7521716acd90dfafbb34f24cda3995f6a49bbabf
Bryan Heden <b.heden@gmail.com>
2020-10-11 22:52:42 -0500

    starting fresh

--- builder/template-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get template vars from specified file (%%%{VARNAME})
-# and print to stdout
-
-file=$1
-
-cat "${file}" | grep "%%%{" | sed 's/.*%%%{//;s/}.*//' | sort | uniq
--- publisher/blog.cron.d
+++
@@ -1,4 +0,0 @@
-
-# set config options in /var/www/publisher/config
-
-* * * * * www-data /bin/bash /var/www/publisher/publisher.sh >/var/www/publisher/bloglog 2>&1
